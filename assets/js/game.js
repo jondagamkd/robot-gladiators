3c778635a2c0ba4d1c815d95fb7aab31c40b4720
jondagamkd <johntaylorwoodland@gmail.com>
2021-08-01 19:19:43 -0700

    added to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! Javascript is running!");
+// this creates a function named "fight"
+
+var playername = window.prompt("What is your robot's name?");
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+console.log(playername);
+
+  //fight();
